172f44b25e8c827c272b681c30d84ffe98244a04
DBVeit <davi.veit@pucpr.edu.br>
2024-09-07 22:21:23 -0300

    Dashboard exemplo + Corporacao e setor

---
+++ src/components/DashboardChamados.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ticket-form-container">
+    <h1>Meus chamados</h1>
+    <div class="filter">
+      Filtros:
+      <select v-model="selectedStatus" @change="filterChamados">
+        <option selected disabled>Selecionar...</option>
+        <option value="1">Em aberto</option>
+        <option value="2">Em atendimento</option>
+        <option value="3">Respondido</option>
+        <option value="4">Concluido</option>
+        <option value="0">Cancelado</option>
+      </select>
+      <a href="" @click.prevent="limparFiltros" v-if="selectedStatus !== null"
+        >Limpar filtros</a
+      >
+    </div>
+  </div>
+</template>
+<script></script>
